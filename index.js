cc462656fcae6a213cb6e13fcbafe4915bc491b6
Charles Stevens <charles.stevens88@gmail.com>
2019-10-13 09:56:17 -0400

    Add endpoints for GET and PUT chains, create component directories for chain functionality

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const pool = pg.initializePostgres();
 // It doesn't belong here
 
 // TODO: Move these routes into their own module
-app.get('/getData', (req, res) => {
+app.get('/getUsers', (req, res) => {
     pool.query('SELECT * FROM users', (error, results) => {
         if(error) {
             throw error
@@ -25,6 +25,28 @@ app.put('/createUser', (req, res) => {
     const password = req.query.password;
     const email = req.query.email;
     pool.query('INSERT INTO users (id, username, password, email) VALUES ($1, $2, $3, $4)', [uuid.v1(), username, password, email], (error, results) => {
+        if(error) {
+            throw error;
+        }
+        res.status(201).json(results.rows);
+    })
+});
+
+app.get('/getChains', (req, res) => {
+    pool.query('SELECT * FROM chains', (error, results) => {
+        if(error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+});
+
+app.put('/createChain', (req, res) => {
+    const title = req.query.title;
+    const description = req.query.description || ''; // This is optional, can be NULL
+    const color = req.query.color || 'blue'; // Optional but NOT NULL, defaults to blue
+    pool.query('INSERT INTO chains (id, title, description, color) ' + 
+    'VALUES ($1, $2, $3, $4)', [uuid.v1(), title, description, color], (error, results) => {
         if(error) {
             throw error;
         }

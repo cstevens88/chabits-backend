58f4119ad765f5a07acac6bd8d4e563165080804
Charles Stevens <charles.stevens88@gmail.com>
2019-10-15 11:42:53 -0400

    Cleanup, logging work

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const bodyParser = require('body-parser');
 const expressWinston = require('express-winston');
 const winston = require('winston');
-
 const express = require('express');
 const app = express();
 const port = 3000;
 
-const userRoutes = require('./components/users/usersAPI');
 const chainRoutes = require('./components/chains/chainsAPI');
+const userRoutes = require('./components/users/usersAPI');
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(bodyParser.json())
-
 app.use(expressWinston.logger({
     transports: [
-      new winston.transports.Console()
-    ],
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.json(),
-      winston.format.prettyPrint()
-    ),
-    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
-}));
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/info.log', level: 'info'})
+      ],
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json(),
+  
+      ),
+      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+      msg: "HTTP {{req.method}} {{req.url}} {{res.responseTime}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+      expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+      colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+      ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+  }));
 
 app.use('/users', userRoutes);
 app.use('/chains', chainRoutes);

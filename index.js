8c3e271930b5915cab5779ad1cdae25264cbaf42
Charles Stevens <charles.stevens88@gmail.com>
2019-10-15 13:28:09 -0400

    Rename logger

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const myLogger = require('./logConfig').myLogger;
+const myLogger = require('./logger').myLogger;
 
 const chainRoutes = require('./components/chains/chainsAPI');
 const userRoutes = require('./components/users/usersAPI');

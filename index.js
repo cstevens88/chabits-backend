ad4612460f19811a886756bd033eb021a6b13dc2
Charles Stevens <charles.stevens88@gmail.com>
2019-10-17 12:37:44 -0400

    Connected to RDS instance from app

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 
 const myLogger = require('./logger').myLogger;
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.POSTGRES_DATABASE,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
+    {
+        host: process.env.POSTGRES_HOST,
+        dialect: 'postgres'
+    }
+);
+
+    sequelize
+        .authenticate()
+        .then(() => {
+            console.log('Connected to RDS instance')
+        })
+        .catch(err => {
+            console.error('Error connecting to RDS instance', err);
+        });
 
 const chainRoutes = require('./components/chains/chainsAPI');
 const userRoutes = require('./components/users/usersAPI');

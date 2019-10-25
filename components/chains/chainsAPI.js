7c9c1c912c45b42eda51675b2f9b654dcfa14098
Charles Stevens <charles.stevens88@gmail.com>
2019-10-24 20:00:45 -0400

    Remove this for now; it's causing some weird error

--- components/chains/chainsAPI.js
+++ components/chains/chainsAPI.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Sequelize = require('Sequelize');
-const Op = Sequelize.Op;
 
 /*router.get('/', (req, res) => {
     pool.query('SELECT * FROM chains', (error, results) => {
